6c2c211bad9f24014a69a532f84494305700aa43
Tor Hveem <tor@hveem.no>
2012-11-12 15:24:29 +0100

    moz and ff spinner

--- static/ngphotox.js
+++ static/ngphotox.js
@@ -29,24 +29,19 @@ $(function(){
         If the lightbox window HTML doesn't exists, create it and insert it.
         (This will only happen the first time around)
         */
+        $('.spinner').removeClass('hidden');
         
         if ($('#lightbox').length > 0) { // #lightbox exists
-            
+
             //place href as img src value
             $('#content').html('<img src="' + image_href + '" />');
             
-            //show lightbox window - you could use .show('fast') for a transition
-            $('#lightbox').show();
 
-            // effects for background
-            $('.items').addClass('backgrounded');
-        }
-        
-        else { //#lightbox does not exist - create and insert (runs 1st time only)
+        } else { //#lightbox does not exist - create and insert (runs 1st time only)
             
             //create HTML markup for lightbox window
             var lightbox = 
-            '<div id="lightbox">' +
+            '<div id="lightbox" class="hidden">' +
                 '<p><i class="icon-resize-small"></i></p>' +
                 '<div id="content">' + //insert clicked link's href into img src
                     '<img src="' + image_href +'" />' +
@@ -57,26 +52,31 @@ $(function(){
             $('body').append(lightbox);
 
         }
-        
+        $('#content').imagesLoaded(function( $images, $proper, $broken ) {
+            console.log('hello from lb loaded');
+            // effects for background
+            $('.items').addClass('backgrounded');
+            //show lightbox window - you could use .show('fast') for a transition
+            $('#lightbox').removeClass('hidden').show();
+            $('.spinner').addClass('hidden');
+        });
+
     });
+        
     var hideLB = function() {
-        $('#lightbox').hide();
         // effects for background
         $('.items').removeClass('backgrounded');
+        //$('#lightbox').hide();
+        $('#lightbox').hide();
     };
     
     //Click anywhere on the page to get rid of lightbox window
     $('#lightbox').live('click', hideLB);  //must use live, as the lightbox element is inserted into the DOM
 
-    window.document.onkeydown = function (e)
-    {
+    window.document.onkeydown = function (e) {
       if (!e)
           e = event;
     if (e.keyCode == 27)
         hideLB();
     }
-
-
-
-
-})
+});
