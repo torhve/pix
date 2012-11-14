477445ad4d02db8830075c127ffc6fec14cc77ea
Simen Graaten <simeng@gmail.com>
2012-11-14 21:32:53 +0100

    Added config file and move redis setup to config

--- bin/worker.py
+++ bin/worker.py
@@ -3,23 +3,31 @@
 from redis import Redis
 from PythonMagick import Image
 import os
+from os.path import sep
 import sys
+import json
 
 class Worker:
-    def __init__(self):
-        self.redis = Redis(unix_socket_path='/var/run/redis/redis.sock')
+    def __init__(self, config):
+        if 'redis' in config:
+            # TODO support sockets and stuffs
+            if 'unix_socket_path' in config["redis"]:
+                self.redis = Redis(unix_socket_path = config["redis"]["unix_socket_path"])
 
     def fetch_thumb_job(self):
         return self.redis.blpop('queue:thumb')[1]
 
-    def thumbnail(infile, outfile, size):
-        image = Image(filename)
+    def thumbnail(self, infile, outfile, size):
+        image = Image(infile)
         image.resize(size)
         image.write(outfile)
 
 if __name__ == '__main__':
     BASE_DIR = os.path.dirname(__file__) + "/.."
-    w = Worker()
+    with open(BASE_DIR + sep + "etc" + sep + "config.json") as f:
+        config = json.loads(f.read())
+
+    w = Worker(config)
     key = w.fetch_thumb_job()
 
     print key
