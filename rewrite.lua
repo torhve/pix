0b773b0b906e03f4776af88c65b496dcf620a8cf
Tor Hveem <tor@hveem.no>
2013-11-24 20:11:34 +0100

    modernize rewrite

--- rewrite.lua
+++ rewrite.lua
@@ -1,3 +1,18 @@
+local redis = require "resty.redis"
+local persona = require 'persona'
+
+local config = ngx.shared.config
+
+-- Only load config once. TODO Needs a /reload url to reload config / unset it.
+if not config then
+    local f = assert(io.open(ROOT_PATH .. "/etc/config.json", "r"))
+    local c = f:read("*all")
+    f:close()
+
+    config = cjson.decode(c)
+    ngx.shared.config = config
+end
+
 local function verify_access_key(red, key, album)
     local accesskey = 'album:' .. album .. ':' .. key
     local exists = red:exists(accesskey) == 1
@@ -17,26 +32,31 @@ local function exit(red, status)
     ngx.exit(status)
 end
 
-BASE = '/'
---local cjson = require "cjson"
---ngx.log(ngx.ERR, cjson.encode(match))
-local redis = require "resty.redis"
+-- helper function to verify that the current user is logged in and valid
+-- using persona
+local function is_admin() 
+    if persona.get_current_email() == config.admin then
+        return true
+    end
+    return false
+end
+
+BASE = config.path.base
 local red = redis:new()
 
 local match = ngx.re.match(ngx.var.uri, "^/(album|img)/(\\w+)/([a-zA-Z0-9-_\\.]+)/(.*)?/?", "o")
 if match then 
     local urltype = match[1]
     local key     = match[2]
-    --local tag     = match[3]
     local album   = match[3]
     local img     = match[4]
 
-    local ok  = red:connect("unix:/var/run/redis/redis.sock")
+    local ok, err = red:connect("unix:" .. config.redis.unix_socket_path)
 
-    local verified = nil
+    local verified = false
   
     -- IP based admin access
-    if ngx.var.remote_addr == "172.16.36.100" then
+    if is_admin() then
         verified = true
     else
         verified = verify_access_key(red, key, album) 
