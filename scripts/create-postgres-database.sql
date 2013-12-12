48b9bd6142ceabafe932fffe799a5e99c5fde2fc
Tor Hveem <tor@hveem.no>
2013-12-12 16:53:06 +0100

    fix extension

--- scripts/create-postgres-database.sql
+++ scripts/create-postgres-database.sql
@@ -1,13 +1,14 @@
-CREATE EXTENSION hstore;
 UPDATE pg_database SET datistemplate = FALSE WHERE datname = 'template1';
 DROP DATABASE template1;
 CREATE DATABASE template1 WITH TEMPLATE = template0 ENCODING = 'UTF-8';
 UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template1';
 UPDATE pg_database SET datallowconn = FALSE WHERE datname = 'template1';
 
-CREATE USER pix WITH PASSWORD 'pixplzpixplzpixplz';
+CREATE EXTENSION hstore;
 
 DROP DATABASE IF EXISTS pix;
 CREATE DATABASE pix WITH ENCODING 'UTF8';
+
+CREATE USER pix WITH PASSWORD 'pixplzpixplzpixplz';
 
 GRANT ALL PRIVILEGES ON DATABASE pix to pix;
