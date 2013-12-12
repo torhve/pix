d46ed43f8f52830b21dfbfdb1a799e841ea0cb50
Tor Hveem <tor@hveem.no>
2013-12-12 16:33:08 +0100

    fix encoding

--- scripts/create-postgres-database.sql
+++ scripts/create-postgres-database.sql
@@ -1,4 +1,10 @@
 CREATE EXTENSION hstore;
+UPDATE pg_database SET datistemplate = FALSE WHERE datname = 'template1';
+DROP DATABASE template1;
+CREATE DATABASE template1 WITH TEMPLATE = template0 ENCODING = 'UTF-8';
+UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template1';
+UPDATE pg_database SET datallowconn = FALSE WHERE datname = 'template1';
+
 CREATE USER pix WITH PASSWORD 'pixplzpixplzpixplz';
 
 DROP DATABASE IF EXISTS pix;
